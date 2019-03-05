480bbd7432bec0e27a60149586be6fb838de77b6
benedict.law <benedict.law@gmail.com>
2019-03-05 00:24:30 -0500

    Module1 demo.

---
+++ Module1.CSharpDemos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 2;
+
+            int secondVar;
+            secondVar = 10;
+
+            int sum = myVar + secondVar;
+            int product = myVar * secondVar;
+            int remainder = myVar % secondVar;
+            double value = (double)myVar / (double)secondVar;
+
+
+            Console.WriteLine(sum);
+            Console.WriteLine(product);
+            Console.WriteLine(remainder);
+            Console.WriteLine(value);
+
+
+            string firstName = "Tom";
+            string lastName = "Sawyer";
+
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine("The full name is {0} {1}", firstName, lastName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
