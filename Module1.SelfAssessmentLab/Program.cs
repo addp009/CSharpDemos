4ffc49165176686758493539a049d8274a7ba424
benedict.law <benedict.law@gmail.com>
2019-03-05 00:48:27 -0500

    Module 1 self lab.

---
+++ Module1.SelfAssessmentLab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1.SelfAssessmentLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // student
+            string studentFirstName;
+            string studentLastName;
+            DateTime studentBirthDate;
+            string studentAddressLine1;
+            string studentAddressLine2;
+            string studentCity;
+            string studentState;
+            string studentZip;
+            string studentCountry;
+
+            // UProgram Info
+            string uProgramName;
+            string uProgramDepartmentHead;
+            int uProgramDegrees;
+
+            // degree Info
+            string degreeName;
+            double creditRequired;
+
+            // Course Info
+            string courseName;
+            double courseCredits;
+            int durationWeek;
+            string courseTeacher;
+
+        }
+    }
+}
