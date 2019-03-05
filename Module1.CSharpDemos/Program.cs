400a249d2812358babc0b9b44854dab19f4c6eab
benedict.law <benedict.law@gmail.com>
2019-03-05 00:37:52 -0500

    Module1 tutorial lab.

--- Module1.CSharpDemos/Program.cs
+++ Module1.CSharpDemos/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpDemos1
+namespace Module1.CSharpDemos
 {
     class Program
     {
---
+++ Module1.TutorialLab/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1.TutorialLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create variables of different data types
+            // initialize with a "default" value
+
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            // Assign some values
+            firstName = "Tom";
+            lastName = "Thumb";
+            age = 18;
+            street = "123 Forth Street";
+            city = "Anytown";
+            country = "MyCountry";
+            birthDate = new DateTime(1932, 6, 1);
+
+            // output to console windown
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            // placeholder style
+            Console.WriteLine("{0} years old.", age);
+
+            // string concatenation
+            Console.WriteLine(street + ", " + city + " ," + country);
+
+            // string interpolation
+            Console.WriteLine($"Born on {birthDate}");
+
+        }
+    }
+}
